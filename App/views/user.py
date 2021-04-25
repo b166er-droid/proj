06e4bdbee69eb74cb6972cd457a78ddee2b5b96e
b166er-droid <j.sookram123@gmail.com>
2021-04-25 04:29:06 -0400

    Revert "b"
    This reverts commit 72f90af1c820253af2a9fa528ccb29c6be4b192d.

--- App/views/user.py
+++ App/views/user.py
@@ -3,18 +3,19 @@ from flask import Blueprint, redirect, render_template, request, jsonify, send_f
 user_views = Blueprint('user_views', __name__, template_folder='../templates')
 
 from App.models import User
-from App.controllers import ( get_users, get_users_json, create_user )
 
 @user_views.route('/users', methods=['GET'])
 def get_user_page():
     users = User.query.all()
     return render_template('users.html', users=users)
 
-@user_views.route('/api/users', methods=['POST'])
-def create_user_action():
-    data = request.json
-    create_user(data['first_name'], data['last_name'])
-    return 'Created'
+@user_views.route('/api/users')
+def client_app():
+    users = User.query.all()
+    if not users:
+        return jsonify([])
+    users = [user.toDict() for user in users]
+    return jsonify(users)
 
 @user_views.route('/static/users')
 def static_user_page():
